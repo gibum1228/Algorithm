5189e553d1fd4e485038b8477479b8ab0f6f5a6d
gibum1228 <gibum1228@naver.com>
2020-03-30 10:58:59 +0900

    숫자 맞추기 프로그램

--- src/hw/NumberCorrect.java
+++ src/hw/NumberCorrect.java
@@ -1,4 +1,9 @@
-// 201814066 김기범
+/*
+ * 파일명: NumberCorrect.java
+ * 작성자: 김기범
+ * 작성일: 2020.03.26.
+ * 내용: 알고리즘 과제 1이며 사용자에게 정수값을 입력받아 주어진 정수 배열과 얼마나 가까운 가를 점수로 출력한다.
+ */
 package hw;
 
 import java.util.Scanner;
@@ -16,13 +21,16 @@ public class NumberCorrect {
 		int score = 100; // 점수
 		
 		for(int i = 0; i < intList.length; i++) {
-			if(inputN == intList[i]) { // 같은 정수값이 있기 때문에 100점
+			if(inputN == intList[i]) { // 정수 배열에 입력받은 정수값과 동일할 경우 100점
 				break;
-			}else if(i == 0){ // 입력받은 값이 -30보다 작을 경우
+			}else if(i == 0){ // 입력 받은 값이 -30보다 작을 경우
 				if(inputN < intList[i]) {
 					score = score - (intList[0] - inputN);
 					break;
 				}
+			}else if(i == (intList.length - 1) && inputN > intList[i]){ // 입력 받은 값이 12345보다 클 경우
+				score = score - (inputN - intList[i]);
+				break;
 			}else {
 				if(inputN < intList[i]) { // 가까운 수를 찾고 점수를 계산
 					if(inputN - intList[i-1] < intList[i] - inputN) { // 왼쪽 정수값과 더 가까울 경우
